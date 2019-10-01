935df03670c65e0f92271d63c97abe3a11c32035
roshangit-repo <dibyapatra48@gmail.com>
2019-10-01 07:38:32 +0530

    added git-demo project to local repo

---
+++ src/main/java/com/javatechie/git_demo/App.java
@@ -0,0 +1,17 @@
+package com.javatechie.git_demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{	
+	public void addNumbers(int sum) {
+		System.out.println("Sum"+sum);
+	}
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
