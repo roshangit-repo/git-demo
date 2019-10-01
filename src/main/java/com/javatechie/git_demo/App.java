8a12af4e35023c2741b6354567e46dc21c192eae
roshangit-repo <dibyapatra48@gmail.com>
2019-10-01 07:56:05 +0530

    added if condition to check the sum

--- src/main/java/com/javatechie/git_demo/App.java
+++ src/main/java/com/javatechie/git_demo/App.java
@@ -8,7 +8,11 @@ public class App
 {	
 	public void addNumbers(int sum) {
 		System.out.println("Sum"+sum);
+		if(sum==10) {
+			System.out.println("sum is "+10);
+		}
 	}
+	
 	
     public static void main( String[] args )
     {
